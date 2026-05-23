edca81b724515d9ba9a51a34c77c2e22079c5a4f
Okikiola <okikday@gmail.com>
2026-05-23 01:51:28 +0100

    feat: Add solution for problem 1752 with check method implementation

---
+++ dart/1752.dart
@@ -0,0 +1,26 @@
+class Solution {
+  bool check(List<int> nums) {
+    final head = 0;
+    final tail = nums.length - 1;
+    int max = tail;
+
+    while (nums[tail] <= nums[head] && max >= 0) {
+      final tmp = nums[tail];
+      nums.removeAt(tail);
+      nums.insert(head, tmp);
+      max--;
+    }
+
+    for (int i = 1; i <= tail; i++) {
+      if (nums[i - 1] > nums[i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+// void main() {
+//   print("${Solution().check([3, 2, 1])}");
+// }
